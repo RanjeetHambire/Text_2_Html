e5bbbaade08df44a0971d44c1d00d902f705bb73
Ranjeet Hambire <ranjeethambire96@gmail.com>
2022-10-16 12:18:18 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Text2List
+    A webpage for converting a list to HTML tag items.
+
+
+## How To Use
+
+    1. Give the random text.
+    2. Choose appropriate on cheackbox and radio buttons.
+    3. Click the convert list button.
+    4. HTML tag are generated.
+
---
+++ script.js
@@ -0,0 +1,69 @@
+function getSeperator() {
+    var radioButtons = document.querySelector('input[name="separate-by"]:checked').value;
+    switch(radioButtons) {
+      case "separate-new-lines":
+        return /\n/g;
+      case "separate-commas":
+        return ","
+      case "separate-custom":
+        return document.getElementById("separate-custom-input").value;
+    }
+  }
+  
+  function getTag() {
+    var radioButtons = document.querySelector('input[name="display-as"]:checked').value;
+    switch(radioButtons) {
+      case "html-list":
+        return "li";
+      case "html-select":
+        return "option"
+      case "html-custom":
+        return document.getElementById("custom-tag").value;
+    }
+  }
+  
+  function getValue() {
+    var radioButtons = document.querySelector('input[name="value-format"]:checked').value;
+    switch(radioButtons) {
+      case "no-value":
+        return "none";
+      case "first-word":
+        return "first"
+      case "join-using":
+        return document.getElementById("custom-join").value;
+    }
+  }
+  
+  function convertList() {
+    var textInput = document.getElementById("text-input").value.trim();
+    var convertInput = document.getElementById("convert-input");
+  
+    var seperator = getSeperator();
+    var textArray = textInput.split(seperator);
+  
+    textArray = document.getElementById("remove-duplicates").checked ? [...new Set(textArray)] : textArray
+  
+    var tag = getTag();
+  
+    var value = getValue();
+  
+    var convertArray;
+  
+    if (value == "none") {
+      convertArray = textArray.map(item => `<${tag}>${item.trim()}</${tag}>`);
+    } else if (value=="first") {
+      convertArray = textArray.map(item => `<${tag} value="${item.trim().toLowerCase().split(" ")[0]}">${item.trim()}</${tag}>`)
+    } else {
+      convertArray = textArray.map(item => `<${tag} value="${item.trim().toLowerCase().split(" ").join(value)}">${item.trim()}</${tag}>`)
+    }
+    
+    convertInput.value = tag == "option" ? `<select>\n  ${convertArray.join("\n  ")}\n</select>` : convertArray.join("\n")
+  }
+  
+  function copyConvertedList() {
+    var convertInput = document.getElementById("convert-input");
+    convertInput.select();
+    if(document.execCommand("copy")){
+        alert("Copied to clipboard.");
+    };
+  }
